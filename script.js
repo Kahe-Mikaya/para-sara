23328ab57a9b4ac98c5dc7a852c232e78bfecf8f
Kahe Mikáyas <132231345+Kahe-Mikaya@users.noreply.github.com>
2024-08-05 14:17:16 +0000

    terminando as palavras e a mensagem

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 let alfabeto = "abcdefghijklmnopqrstuvwxyz"; 
-let palavras = [{"palavra":"abacate","dica":"fruta"},{"palavra":"serenata-existencialista","dica":"musica"} ];
+let palavras = [{"palavra":"preto","dica":"minha cor preferida \n(muito facil essa)"},
+  {"palavra":"lenine","dica":"qual é o nome da minha da minha gata\n  (impossivel nao saber)"},
+  {"palavra":"serenata-existencialista","dica":"nossa musica musica"},
+  {"palavra":"judith","dica":"uma amiga minha "},
+  {"palavra":"macarronada","dica":"minha comida preferida"},
+  {"palavra":"","dica":"Meu amor,Não consigo acreditar que já estamos completando um mês juntos, porque parece que te conheço há muito mais tempo. Cada dia ao seu lado tem sido uma verdadeira alegria. Você é incrível, linda, maravilhosa e doce, e minha vida se transformou para melhor desde que você entrou nela. Eu estou tão feliz por ter encontrado você e não posso esperar para passar o resto da minha vida ao seu lado. Espero que você nao se enjoe de mim para que em breve, possamos nos casar e viver juntos, e ter nossos gatinhos e compartilhar muitos outros momentos,"} ];
 let tentativa = 0;
 let palavra = "";
 let palavra_secreta = "";
@@ -34,7 +39,7 @@ function gerarBotao(){
         erros++;
         document.querySelector("#forca").src = imagens[erros];
         if(erros == 6){
-          alert("Você perdeu!");
+          alert("Você perdeu! ;-;");
           document.querySelector("#palavra").textContent = palavra;
           document.querySelector("#pergunta").textContent = "A palavra era: " + palavras[tentativa].palavra;
           console.log(palavras[tentativa].palavra);
@@ -52,7 +57,9 @@ function gerarBotao(){
 } 
 
 function codificarPalavra(){
-  gerarBotao();
+  if(tentativa<5){
+    gerarBotao();
+  }
   div_palavra.textContent= "";
   palavra = "";
   for (let i = 0; i < palavras[tentativa].palavra.length; i++){
@@ -84,7 +91,7 @@ function tentarLetra(letra){
   console.log(palavra, palavra_secreta)
   div_palavra.textContent = palavra;
   if (palavra_secreta == palavras[tentativa].palavra){
-    alert("voce acertou!\n a palavra era: " + palavras[tentativa].palavra);
+    alert("voce acertou amor!\n a palavra era: " + palavras[tentativa].palavra,"");
     tentativa++;
     codificarPalavra();
   }
